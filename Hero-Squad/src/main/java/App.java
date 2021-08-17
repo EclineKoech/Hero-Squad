ff53798af0f39737c3c88fb0ec4262723f3e2e51
Ecline Koech <eclinejerutokoech@gmail.com>
2021-08-17 22:20:32 +0300

    add /form route

--- Hero-Squad/src/main/java/App.java
+++ Hero-Squad/src/main/java/App.java
@@ -1,7 +1,16 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
-    get("/hello",(request, response) -> "Hello Friend");
+    get("/form",(request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      return new ModelAndView(model, "forms.hbs");
+    }, new HandlebarsTemplateEngine());
   }
 }
