4d9888e82a7a5ae96430f459bfbbb610531ec4f6
Ecline Koech <eclinejerutokoech@gmail.com>
2021-08-23 11:31:33 +0300

    rename hero class

--- Hero-Squad/src/main/java/models/Hero.java
+++ Hero-Squad/src/main/java/models/Hero.java
@@ -2,28 +2,106 @@ package models;
 
 import java.util.ArrayList;
 
+
 public class Hero {
-  private String content;
+  private static Object Hero;
+  private String name;
+  private int age;
+  private String power;
+  private String weakness;
   private static ArrayList<Hero> instances = new ArrayList<>();
-    public Hero(String content) {
-      this.content = content;
-      instances.add(this);
-    }
 
   public Hero(String name, int age, String power, String weakness) {
+    this.name = name;
+    this.age = age;
+    this.power = power;
+    this.weakness = weakness;
   }
 
-  public String getContent(){
-      return content;
-    }
-  public static void main(String[] args) {
+  public static Object getAll() {
+    return (Object) Hero;
+  }
 
+  public String getName() {
+    return name;
   }
-  public static ArrayList<Hero> getAll(){
-    ArrayList<Hero> instances = new ArrayList<>();
-    return instances;
+
+  public void setName(String name) {
+    this.name = name;
   }
-  public static void clearAllPosts(){
-      instances.clear();
+
+  public int getAge() {
+    return age;
   }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String getPower() {
+    return power;
+  }
+
+  public void setPower(String power) {
+    this.power = power;
+  }
+
+  public String getWeakness() {
+    return weakness;
+  }
+
+  public void setWeakness(String weakness) {
+    this.weakness = weakness;
+  }
+
+  public Hero() {
+    super();
+  }
+
 }
+
+
+//
+//  public Hero(String hero) {
+//      this.hero = hero;
+//      instances.add(this);
+//    }
+//
+//  public Hero(String hero, Object hero1) {
+//    this.hero = hero;
+//    Hero = hero1;
+//  }
+//
+//  public void setHero(String hero) {
+//    this.hero = hero;
+//  }
+//
+//  public static ArrayList<models.Hero> getInstances() {
+//    return instances;
+//  }
+//
+//  public static void setInstances(ArrayList<models.Hero> instances) {
+//    Hero.instances = instances;
+//  }
+//
+//  public void setHero(Object hero) {
+//    Hero = hero;
+//  }
+//
+//  public Hero(String name, int age, String power, String weakness) {
+//  }
+//
+//  public String getHero(){
+//      return (String) Hero;
+//    }
+//  public static void main(String[] args) {
+//
+//  }
+//  public static ArrayList<Hero> getAll(){
+//    ArrayList<Hero> instances = new ArrayList<>();
+//    return instances;
+//  }
+//  public static void clearAllPosts(){
+//      instances.clear();
+//  }
+//}
